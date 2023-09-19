ca87d4f65c439bf9b0274939974265bdd4b11c4e
Max Hirtenstein <maxhirtens@gmail.com>
2023-09-19 14:35:02 -0700

    changed extract function

--- backend/helpers/extract.js
+++ backend/helpers/extract.js
@@ -3,12 +3,16 @@
 // Helper function to clean up articles routes.
 
 function extract(articles) {
-  articles = articles.data;
-  let selects = articles.results;
-  let top3 = selects.slice(0, 3);
-  let top3Data = top3.map((c) => c.title + ": " + c.abstract);
-  let top3joined = top3Data.join(" ");
-  return top3joined;
+  try {
+    articles = articles.data;
+    let selects = articles.results;
+    let top3 = selects.slice(0, 3);
+    let top3Data = top3.map((c) => c.title + ": " + c.abstract);
+    let top3joined = top3Data.join(" ");
+    return top3joined;
+  } catch (err) {
+    return next(err);
+  }
 }
 
 module.exports = {
